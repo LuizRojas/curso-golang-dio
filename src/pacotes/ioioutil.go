a69562199427a857ff214df82b8eb4cdc6dc50d2
LuizRojas <luizfrojas@gmail.com>
2025-10-17 01:56:18 -0300

    pacotes io error path e os

--- src/pacotes/ioioutil.go
+++ src/pacotes/ioioutil.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 )
 
@@ -8,6 +9,6 @@ func main() {
 	message := []byte("Hello, Gophers!")
 	err := os.WriteFile("Teste.txt", message, 0644)
 	if err != nil {
-		
+		log.Fatal(err)
 	}
 }
